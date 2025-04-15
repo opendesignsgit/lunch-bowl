521af8d39386f20c3cf356c0fb55f7af40f27769
RJSivaODI <sivashanmugam@opendesignsin.com>
2025-04-15 12:40:37 +0530

    adding home page

---
+++ store/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function home() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a basic React functional component.</p>
+    </div>
+  );
+}
+
+export default home;
