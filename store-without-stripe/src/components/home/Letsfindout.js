74f1b299bf252a48c4011a9b859f13c0af2c2188
RJSivaODI <sivashanmugam@opendesignsin.com>
2025-04-28 17:48:49 +0530

    Menu Updates

--- store-without-stripe/src/components/home/Letsfindout.js
+++ store-without-stripe/src/components/home/Letsfindout.js
@@ -22,11 +22,27 @@ const Letsfindout = () => {
       <div className="lfooutfild">
         <h6>Select a school to check its availability.</h6>
         <div className="inputbox bg-white p-[10px] rounded-[10px] flex ">
-          <select name="schoolname">
+        <select name="schoolname">
             <option>Select School Name</option>
-            <option value="OptionOne">option 1</option>
-            <option value="OptionTwo">option 2</option>
-            <option value="OptionThree">option 3</option>
+            <option value="Asan Memorial Senior Secondary School">Asan Memorial Senior Secondary School</option>
+            <option value="D.A.V. Public School">D.A.V. Public School</option>
+            <option value="Don Bosco Matriculation Higher Secondary School">Don Bosco Matriculation Higher Secondary School</option>
+            <option value="Holy Angels Anglo Indian Higher Secondary School">Holy Angels Anglo Indian Higher Secondary School</option>
+            <option value="KC High School">KC High School</option>
+            <option value="MCTM International School">MCTM International School</option>
+            <option value="P.S. Higher Secondary School">P.S. Higher Secondary School</option>
+            <option value="Padma Seshadri Bala Bhavan School">Padma Seshadri Bala Bhavan School</option>
+            <option value="Sishya School">Sishya School</option>
+            <option value="St. Patrick's Anglo Indian Higher Secondary School">St. Patrick's Anglo Indian Higher Secondary School</option>
+            <option value="Vidya Mandir Senior Secondary School, Mylapore">Vidya Mandir Senior Secondary School, Mylapore</option>
+            <option value="Bala Vidya Mandir">Bala Vidya Mandir</option>
+            <option value="Sharanalaya Montessori School">Sharanalaya Montessori School</option>
+            <option value="Chetinad Harishree Vidyalayam">Chetinad Harishree Vidyalayam</option>
+            <option value="Vidyodaya School">Vidyodaya School</option>
+            <option value="Sprouts">Sprouts</option>
+            <option value="St Michael Academy">St Michael Academy</option>
+            <option value="Accord International School">Accord International School</option>
+            <option value="St Johns English School">St Johns English School</option>
           </select>
           <button className="btn">
             <span>Submit</span>
